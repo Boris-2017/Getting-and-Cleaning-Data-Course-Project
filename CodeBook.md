92b07a4810451e5e28e30b0e93e0d7a5736c280a
Boris <Boris-2017@users.noreply.github.com>
2017-06-24 12:22:02 +0100

    v1

---
+++ CodeBook.md
@@ -0,0 +1,143 @@
+# Codebook for Getting and Cleaning Data Course Project on Coursera.org: Samsung Wearable Data
+
+## Introduction
+
+The variables described below are the IDs and summaries of data collected from accelerometers and gyroscopes of Samsung Galaxy S smartphones during execution of six different types of activity by 30 volunteers. The original data set is taken from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and is fully described at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones and in the following publication:
+
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+## Original Data Feature Selection (from README.txt and features_info.txt supplied with the data set)
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+
+The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz. 
+
+Subsequently, the body linear acceleration and angular velocity were derived in time to obtain Jerk signals (tBodyAccJerk-XYZ and tBodyGyroJerk-XYZ). Also the magnitude of these three-dimensional signals were calculated using the Euclidean norm (tBodyAccMag, tGravityAccMag, tBodyAccJerkMag, tBodyGyroMag, tBodyGyroJerkMag). 
+
+Finally a Fast Fourier Transform (FFT) was applied to some of these signals producing fBodyAcc-XYZ, fBodyAccJerk-XYZ, fBodyGyro-XYZ, fBodyAccJerkMag, fBodyGyroMag, fBodyGyroJerkMag. (Note the 'f' to indicate frequency domain signals). 
+
+These signals were used to estimate variables of the feature vector for each pattern:  
+'-XYZ' is used to denote 3-axial signals in the X, Y and Z directions.
+
+tBodyAcc-XYZ
+tGravityAcc-XYZ
+tBodyAccJerk-XYZ
+tBodyGyro-XYZ
+tBodyGyroJerk-XYZ
+tBodyAccMag
+tGravityAccMag
+tBodyAccJerkMag
+tBodyGyroMag
+tBodyGyroJerkMag
+fBodyAcc-XYZ
+fBodyAccJerk-XYZ
+fBodyGyro-XYZ
+fBodyAccMag
+fBodyAccJerkMag
+fBodyGyroMag
+fBodyGyroJerkMag
+
+The set of variables that were estimated from these signals are: 
+
+mean(): Mean value
+std(): Standard deviation
+
+Additional vectors obtained by averaging the signals in a signal window sample. These are used on the angle() variable:
+
+gravityMean
+tBodyAccMean
+tBodyAccJerkMean
+tBodyGyroMean
+tBodyGyroJerkMean
+
+# This Tidy Data Set
+
+The current tidy data set was obtained by:
+1. Combining the test and training data sets described above.
+2. Averaging each mean and each standard deviation variable described above for every combination of activity type and subject. 
+3. Adding subject IDs, descriptive acitvity and variable labels.
+
+The variables below are described in the order in which they appear in the tidy data set. Note that none of these variables have any units: the ID variables are categorical and the Doubleal variables are all normalised between -1 and 1. For the Doubleal variables, a separate decsription is not given as they are all means of the quantity specified by the name of column and described above under Original Data Feature Selection.
+
+#Column No. | #Name | #Type | #Description
+1 | Activity | Category | Subject activity type: one of "laying", "sitting", "standing", "walking", "walking downstairs" and "walking upstairs"
+2 | Subject_ID | Category | Subject identifier: an integer between 1 and 30
+3	|	tBodyAcc-mean()-X	|	Double	|	Between -1 and 1
+4	|	tBodyAcc-mean()-Y	|	Double	|	Between -1 and 1
+5	|	tBodyAcc-mean()-Z	|	Double	|	Between -1 and 1
+6	|	tBodyAcc-std()-X	|	Double	|	Between -1 and 1
+7	|	tBodyAcc-std()-Y	|	Double	|	Between -1 and 1
+8	|	tBodyAcc-std()-Z	|	Double	|	Between -1 and 1
+9	|	tGravityAcc-mean()-X	|	Double	|	Between -1 and 1
+10	|	tGravityAcc-mean()-Y	|	Double	|	Between -1 and 1
+11	|	tGravityAcc-mean()-Z	|	Double	|	Between -1 and 1
+12	|	tGravityAcc-std()-X	|	Double	|	Between -1 and 1
+13	|	tGravityAcc-std()-Y	|	Double	|	Between -1 and 1
+14	|	tGravityAcc-std()-Z	|	Double	|	Between -1 and 1
+15	|	tBodyAccJerk-mean()-X	|	Double	|	Between -1 and 1
+16	|	tBodyAccJerk-mean()-Y	|	Double	|	Between -1 and 1
+17	|	tBodyAccJerk-mean()-Z	|	Double	|	Between -1 and 1
+18	|	tBodyAccJerk-std()-X	|	Double	|	Between -1 and 1
+19	|	tBodyAccJerk-std()-Y	|	Double	|	Between -1 and 1
+20	|	tBodyAccJerk-std()-Z	|	Double	|	Between -1 and 1
+21	|	tBodyGyro-mean()-X	|	Double	|	Between -1 and 1
+22	|	tBodyGyro-mean()-Y	|	Double	|	Between -1 and 1
+23	|	tBodyGyro-mean()-Z	|	Double	|	Between -1 and 1
+24	|	tBodyGyro-std()-X	|	Double	|	Between -1 and 1
+25	|	tBodyGyro-std()-Y	|	Double	|	Between -1 and 1
+26	|	tBodyGyro-std()-Z	|	Double	|	Between -1 and 1
+27	|	tBodyGyroJerk-mean()-X	|	Double	|	Between -1 and 1
+28	|	tBodyGyroJerk-mean()-Y	|	Double	|	Between -1 and 1
+29	|	tBodyGyroJerk-mean()-Z	|	Double	|	Between -1 and 1
+30	|	tBodyGyroJerk-std()-X	|	Double	|	Between -1 and 1
+31	|	tBodyGyroJerk-std()-Y	|	Double	|	Between -1 and 1
+32	|	tBodyGyroJerk-std()-Z	|	Double	|	Between -1 and 1
+33	|	tBodyAccMag-mean()	|	Double	|	Between -1 and 1
+34	|	tBodyAccMag-std()	|	Double	|	Between -1 and 1
+35	|	tGravityAccMag-mean()	|	Double	|	Between -1 and 1
+36	|	tGravityAccMag-std()	|	Double	|	Between -1 and 1
+37	|	tBodyAccJerkMag-mean()	|	Double	|	Between -1 and 1
+38	|	tBodyAccJerkMag-std()	|	Double	|	Between -1 and 1
+39	|	tBodyGyroMag-mean()	|	Double	|	Between -1 and 1
+40	|	tBodyGyroMag-std()	|	Double	|	Between -1 and 1
+41	|	tBodyGyroJerkMag-mean()	|	Double	|	Between -1 and 1
+42	|	tBodyGyroJerkMag-std()	|	Double	|	Between -1 and 1
+43	|	fBodyAcc-mean()-X	|	Double	|	Between -1 and 1
+44	|	fBodyAcc-mean()-Y	|	Double	|	Between -1 and 1
+45	|	fBodyAcc-mean()-Z	|	Double	|	Between -1 and 1
+46	|	fBodyAcc-std()-X	|	Double	|	Between -1 and 1
+47	|	fBodyAcc-std()-Y	|	Double	|	Between -1 and 1
+48	|	fBodyAcc-std()-Z	|	Double	|	Between -1 and 1
+49	|	fBodyAcc-meanFreq()-X	|	Double	|	Between -1 and 1
+50	|	fBodyAcc-meanFreq()-Y	|	Double	|	Between -1 and 1
+51	|	fBodyAcc-meanFreq()-Z	|	Double	|	Between -1 and 1
+52	|	fBodyAccJerk-mean()-X	|	Double	|	Between -1 and 1
+53	|	fBodyAccJerk-mean()-Y	|	Double	|	Between -1 and 1
+54	|	fBodyAccJerk-mean()-Z	|	Double	|	Between -1 and 1
+55	|	fBodyAccJerk-std()-X	|	Double	|	Between -1 and 1
+56	|	fBodyAccJerk-std()-Y	|	Double	|	Between -1 and 1
+57	|	fBodyAccJerk-std()-Z	|	Double	|	Between -1 and 1
+58	|	fBodyAccJerk-meanFreq()-X	|	Double	|	Between -1 and 1
+59	|	fBodyAccJerk-meanFreq()-Y	|	Double	|	Between -1 and 1
+60	|	fBodyAccJerk-meanFreq()-Z	|	Double	|	Between -1 and 1
+61	|	fBodyGyro-mean()-X	|	Double	|	Between -1 and 1
+62	|	fBodyGyro-mean()-Y	|	Double	|	Between -1 and 1
+63	|	fBodyGyro-mean()-Z	|	Double	|	Between -1 and 1
+64	|	fBodyGyro-std()-X	|	Double	|	Between -1 and 1
+65	|	fBodyGyro-std()-Y	|	Double	|	Between -1 and 1
+66	|	fBodyGyro-std()-Z	|	Double	|	Between -1 and 1
+67	|	fBodyGyro-meanFreq()-X	|	Double	|	Between -1 and 1
+68	|	fBodyGyro-meanFreq()-Y	|	Double	|	Between -1 and 1
+69	|	fBodyGyro-meanFreq()-Z	|	Double	|	Between -1 and 1
+70	|	fBodyAccMag-mean()	|	Double	|	Between -1 and 1
+71	|	fBodyAccMag-std()	|	Double	|	Between -1 and 1
+72	|	fBodyAccMag-meanFreq()	|	Double	|	Between -1 and 1
+73	|	fBodyBodyAccJerkMag-mean()	|	Double	|	Between -1 and 1
+74	|	fBodyBodyAccJerkMag-std()	|	Double	|	Between -1 and 1
+75	|	fBodyBodyAccJerkMag-meanFreq()	|	Double	|	Between -1 and 1
+76	|	fBodyBodyGyroMag-mean()	|	Double	|	Between -1 and 1
+77	|	fBodyBodyGyroMag-std()	|	Double	|	Between -1 and 1
+78	|	fBodyBodyGyroMag-meanFreq()	|	Double	|	Between -1 and 1
+79	|	fBodyBodyGyroJerkMag-mean()	|	Double	|	Between -1 and 1
+80	|	fBodyBodyGyroJerkMag-std()	|	Double	|	Between -1 and 1
+81	|	fBodyBodyGyroJerkMag-meanFreq()	|	Double	|	Between -1 and 1
---
+++ run_analysis.R
@@ -0,0 +1,147 @@
+## 18.06.2017: This is the R code for the Getting and Cleaning Data Course Project on coursera.org.
+## The script below operates on the data collected from the accelerometers from the Samsung Galaxy S smartphones
+##      taken from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+##      and described at http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+##  The purpose of the scipt is to prepare a tidy data set from the raw data, suitable for further analysis.
+##
+##  The script stages, following the course project instructions, are:
+##      1. Add activity and subject identifiers to each of training and test datasets (note that both data sets are
+##              assumed to have been downloaded and unzipped without any filename/folder alterations into 
+##              the UCI HAR Dataset folder and the working directory is assumed to have been set to that folder. 
+##      2. Append the two data sets.
+##      3. Extract only the mean and standard deviation of each measured variable in a standard 2.56s window.  
+##      4. Remove any inadmissible values.
+##      5. Give each activity in the data set a descriptive activity name.
+##      6. Give each variable in the data set a descriptive variable name. 
+##      7. Create a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+run_analysis <- function() 
+{
+        
+        ## Output:              Data frame with the following columns:
+        ##                              1. Descriptive activity name (one of six activities)
+        ##                              2. Subject_ID (1 through 30) 
+        ##                              3. For each variable representing the mean of a measurement, its mean for that subject and activity
+        ##                              4. For each variable representing the standard deviation of a measurement, its mean for that subject and activity
+        ##                              Means and standard deviations in (3)--(4) appear in the same order as in the original data,
+        ##                                      and not as first all means followed by all standard deviations.
+        ##                      Note that the script exits with an error if any required file is not found in the expected location.
+
+        
+        # Read in the test data activity_IDs, subject_IDs and the data, and join them along the column dimension 
+        Test_Data=cbind(Load_Text_File("test/y_test.txt"), Load_Text_File("test/subject_test.txt"), Load_Text_File("test/X_test.txt"))   
+        # Same for the training data
+        Training_Data=cbind(Load_Text_File("train/y_train.txt"), Load_Text_File("train/subject_train.txt"), Load_Text_File("train/X_train.txt"))   
+        # Append the test and training data sets to form the full data set
+        Full_Data=rbind(Test_Data, Training_Data)
+        
+        # Read in the descriptive variable names (second column) and ...
+        Variable_Names=Load_Text_File("features.txt")
+        # ... set the Full_Data data frame column names to be the descriptive names. For readability, the descriptive variable names are not converted to lowercase.
+        # Note that the variable names must be strings and hence are converted from factors using as.character
+        colnames(Full_Data)=c("Activity_ID", "Subject_ID", as.character(Variable_Names$V2))
+        
+        # The working data set is the subset of the full data columns representing only the means and standard deviations         
+        Working_Data=Full_Data[, c(1, 2, grep("mean|std", colnames(Full_Data)))]
+        
+        # Now check if there are any inadmissible values within the data against the specifications provided with the data...
+        # .. If any are found, they are replaced with NAs and excluded from the further analysis.
+        Check_for_Inadmissible_Values_Set(Working_Data[, 1], 1:6, "Column Activity ID.")                         
+        Check_for_Inadmissible_Values_Set(Working_Data[, 2], 1:30, "Column Subject ID.")
+        for (i in 3:ncol(Working_Data))
+                Check_for_Inadmissible_Values_Numeric(Working_Data[, i], -1, 1, paste0("Column ", colnames(Working_Data)[i], "."))
+        
+        # Read in the descriptive variable names and ...
+        Activity_Names=Load_Text_File("activity_labels.txt")
+        # ... merge the working data with the descriptive activity names (second column) 
+        Working_Data=merge(Working_Data, Activity_Names, by.x="Activity_ID", by.y="V1")
+        # Now remove the Activity_ID column and make Activity (descriptive name in lowercase for readability) the first column, ...
+        #       ... followed by the rest of the variables. Note that the last column after the merge was ... 
+        #       ... the descriptive activity names, which now has been moved to Column 1 and hence needs to be removed at the end.
+        Working_Data=cbind(Activity=tolower(Working_Data$V2), Working_Data[, 2:(ncol(Working_Data)-1)])
+        
+        # The final tidy data is formed by first splittng the working data into groups defined by subject and activity, then ...
+        Final_Tidy_Data=split(Working_Data[3:ncol(Working_Data)], Working_Data[, 1:2])
+        # ... the mean of each group is computed and the results reassembled into the same columns as before.
+        # Note that by default a list is returned by sapply here and the original columns become rows.
+        Final_Tidy_Data=t(as.data.frame(sapply(Final_Tidy_Data, colMeans)))
+
+        # The row names after the split are of the form Activity.Subject_ID. Hence split the row names at ".", convert to an appropriate ..
+        # ... data frame format and prepend those to the calculated column means.
+        Final_Tidy_Data=cbind(t(as.data.frame(strsplit(rownames(Final_Tidy_Data), ".", fixed=TRUE))), Final_Tidy_Data)
+       
+         # The resulting row names are not very useful, so remove them.
+        rownames(Final_Tidy_Data)=NULL
+        # Add the first two column names since these columns did not have names being derived from the split at "."
+        colnames(Final_Tidy_Data)[1:2]=c("Activity", "Subject_ID")
+        
+        # Finally, for the user's convenience, sort the output by activity first and subject ID (as a number for more natural order) second ...
+        Final_Tidy_Data=Final_Tidy_Data[order(Final_Tidy_Data[, 1], as.numeric(Final_Tidy_Data[, 2])), ]
+        # ... and save it a .txt file as per the course project instructions
+        write.table(Final_Tidy_Data, "Final_Tidy_Data.txt", row.name=FALSE)
+        
+}
+
+        
+## This function simply reads in a text file if it exists. Otherwise, aborts the execution of the entire script.
+
+Load_Text_File <- function (File_Name)
+{
+        
+        ## Input:
+        ##      File_Name:      Name of the text file to be read in
+        ##
+        ## Output:              Data frame with the contents of the file, if file exists
+  
+              
+        if (!file.exists(File_Name))
+                stop(paste("File", File_Name, "not found. Execution aborted.", sep=" "))                
+        read.table(File_Name)
+        
+}
+
+
+## This function checks if inadmissible values are present, issues a warning if so and optionally replaces them with NAs.
+
+Check_for_Inadmissible_Values_Set <- function (Values, Admissible_Set, Warning_Message, Replace_with_NA=TRUE)
+{
+        
+        ## Input:
+        ##      Values:                 Vector of values to check for admissibility
+        ##      Admissible_Set:         Vector of admissible values
+        ##      Warning_Message:        String forming a part of the warning message if inadmissible values are found
+        ##      Replace_with_NAs:       If TRUE, any inadmissible values are replaced with NA. 
+        ##
+        ## Output:                      Vector listing indices of any inadmissible values within Values. Also output as part of the warning message.
+        
+
+        Inadmissible_Value_Indices=which(!(Values %in% Admissible_Set))
+        if (sum(Inadmissible_Value_Indices)>0)
+                print(paste(Warning_Message, "Inadmissible values found at indices:", Inadmissible_Value_Indices))
+        Inadmissible_Value_Indices
+        
+}
+
+
+## This function checks if inadmissible values are present, issues a warning if so and optionally replaces them with NAs.
+
+Check_for_Inadmissible_Values_Numeric <- function (Values, Min, Max, Warning_Message, Replace_with_NA=TRUE)
+{
+        
+        ## Input:
+        ##      Values:                 Vector of values to check for admissibility
+        ##      Min:                    Values must be >= Min to be admissible
+        ##      Max:                    Values must be <= Max to be admissible        
+        ##      Warning_Message:        String forming a part of the warning message if inadmissible values are found
+        ##      Replace_with_NAs:       If TRUE, any inadmissible values are replaced with NA. 
+        ##
+        ## Output:                      Vector listing indices of any inadmissible values within Values. Also output as part of the warning message.
+        
+        
+        Inadmissible_Value_Indices=which(!is.numeric(Values) | Values<Min | Values>Max)
+        if (sum(Inadmissible_Value_Indices)>0)
+                print(paste(Warning_Message, "Inadmissible values found at indices:", Inadmissible_Value_Indices))
+        Inadmissible_Value_Indices
+        
+}
